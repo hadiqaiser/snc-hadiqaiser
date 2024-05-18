efd71bda2db07e8a18cf8fd892effe22a4f57f4a
hadiqaiser <hadiqaiser1996@gmail.com>
2024-05-18 20:49:12 +0500

    feat: setup UserContext and wrap app with UserProvider

---
+++ src/context/actions.ts
@@ -0,0 +1,5 @@
+export enum UserAction {
+  SetEnableLogs = "SET_ENABLE_LOGS",
+  SetSelectedUser = "SET_SELECTED_USER",
+  SetUserData = "SET_USER_DATA",
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// Context
+import { UserProvider } from "@/context/UserContext";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <UserProvider>
+      <Component {...pageProps} />;
+    </UserProvider>
+  );
 }
